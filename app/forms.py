7f1378ca64fa26550b08d21607cf0f29f742c692
Jennifer Qian <jennifer.qian@gmail.com>
2018-02-11 10:56:17 -0500

    final

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import SubmitField, DecimalField
+from wtforms import SubmitField, StringField
 from wtforms.validators import DataRequired
 
 class LatLong(FlaskForm):
-    lat = DecimalField('Latitude', validators=[DataRequired()])
-    long = DecimalField('Longitude', validators=[DataRequired()])
+    start = StringField('Home Zip Code', validators=[DataRequired()])
+    end = StringField('Current Zip Code', validators=[DataRequired()])
+    price = StringField('Price Range', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class Favorites(FlaskForm):
     submit = SubmitField('Submit')
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,29 @@
 from app import app
 from flask import render_template, request, redirect
-from app.forms import LatLong
+from app.forms import LatLong, Favorites
+import requests
+# import graphlab
 
-# @app.route('/')
-# @app.route('/index')
-# def index():
-#     user = {'username': 'Jennifer'}
-#     posts = [
-#         {'author': {'username': 'Bob'}, 'body': 'Hi'},
-#         {'author': {'username': 'Alice'}, 'body': 'Sup'}
-#     ]
-#     return render_template('index.html', title='Home', user=user, posts=posts)
+# import pandas as pa
+#
+# train model
+# restaurants= []
+# train_data = restaurants
+# item_sim_model = graphlab.item_similarity_recommender.create(train_data, target='rating', similarity_type='pearson')
+#
+# make recommendations
+# item_sim_recom = item_sim_model.recommend(users=range(1,6), k = 5)
+# item_sim_recom.print_rows(num_rows=10)
+
+API_KEY = "9FI5hAiEmJXs76PnZeyMydrSa-9eb8-UdG1LloXLifpo7GYjA8kyjg1qOeGzJKGeKmx7QECBdJs3q2IqZhNrTxXk9G_yRfYRton4lxv08bNt8U9sJRs5XRoWYZ1_WnYx"
+
+
+def request_info(api_key, url_params=None):
+    url_params = url_params or {}
+    url = "https://api.yelp.com/v3/businesses/search"
+    headers = {'Authorization': 'Bearer %s' % api_key,}
+    response = requests.request('GET', url, headers=headers, params=url_params)
+    return response.json()['businesses']
 
 @app.route('/user')
 def user_pref():
@@ -19,9 +32,37 @@ def user_pref():
         return redirect('/result')
     return render_template('user_input.html', title='User Preferences', form=form)
 
+def processJSON():
+    form = LatLong()
+    restaurants = []
+    result_start = form.start.data
+    result_price = form.price.data
+    url_params = {'price': result_price, 'location': result_start}
+    result1 = request_info(API_KEY, url_params)
+    for entry in result1:
+        restaurants.append(entry['name'])
+    return restaurants
+
+def processJSON2():
+    restaurants2 = []
+    result_end = LatLong().start.data
+    result_price = LatLong().price.data
+    url_params2 = {'price': '2', 'location': '22903'}
+    result2 = request_info(API_KEY, url_params2)
+    for entry in result2:
+        restaurants2.append(entry['name'])
+    return restaurants2
+
 @app.route('/result', methods=['POST', 'GET'])
 def result():
+    form = Favorites
     if request.method == 'POST':
-        result_lat = request.form['lat']
-        result_long = request.form['long']
-        return render_template('result.html', title='Result', result_lat=result_lat, result_long=result_long)
+        result = processJSON()
+        return render_template('result.html', title='Result', result=result, form=form)
+
+@app.route('/recommend', methods=['POST', 'GET'])
+def recommend():
+    form = Favorites
+    if request.method == 'GET':
+        result = processJSON2()
+        return render_template('recommend.html', title='Result', result=result, form=form)
